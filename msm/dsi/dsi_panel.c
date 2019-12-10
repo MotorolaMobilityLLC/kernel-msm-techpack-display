051a1bf6e9086b331c5de414bcc3771f11e5b4d8
Konstantin Makariev <hcv867@motorola.com>
2019-12-10 13:32:51 -0600

    dsi: pushing display notifications
    Pushing display notifications when it is turning on and off, so
    registered listener can become aware of it state.
    
    Change-Id: I5ae139d5a9099a86d94f54f7495ddf12659c98ba
    Signed-off-by: Konstantin Makariev <hcv867@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1697625
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- msm/dsi/dsi_panel.c
+++ msm/dsi/dsi_panel.c
@@ -10,6 +10,17 @@
 #include <linux/pwm.h>
 #include <video/mipi_display.h>
 
+#if defined(CONFIG_PANEL_NOTIFICATIONS)
+#include <linux/panel_notifier.h>
+
+#define PANEL_NOTIFY(note) { \
+	int idx = dsi_panel_get_index(panel); \
+	panel_notify(note, (void *)&idx); \
+}
+#else
+#define PANEL_NOTIFY(...)
+#endif
+
 #include "dsi_display.h"
 #include "dsi_panel.h"
 #include "dsi_ctrl_hw.h"
@@ -5261,6 +5272,19 @@ int dsi_panel_post_switch(struct dsi_panel *panel)
 	return rc;
 }
 
+#if defined(CONFIG_PANEL_NOTIFICATIONS)
+static inline int dsi_panel_get_index(struct dsi_panel *panel)
+{
+	struct dsi_display *dsi_display =
+			container_of(panel->host, struct dsi_display, host);
+
+	if (unlikely(dsi_display == NULL))
+		return -EINVAL;
+
+	return dsi_display->display_idx;
+}
+#endif
+
 int dsi_panel_enable(struct dsi_panel *panel)
 {
 	int rc = 0;
@@ -5310,6 +5334,9 @@ int dsi_panel_enable(struct dsi_panel *panel)
 		DSI_INFO("-: no_panel_on_read_support is set\n");
 
 	panel->panel_initialized = true;
+
+	PANEL_NOTIFY(PANEL_EVENT_PRE_DISPLAY_ON);
+
 err:
 
 	mutex_unlock(&panel->panel_lock);
@@ -5341,6 +5368,9 @@ int dsi_panel_post_enable(struct dsi_panel *panel)
 			       panel->name, rc);
 		}
 	}
+
+	PANEL_NOTIFY(PANEL_EVENT_DISPLAY_ON);
+
 error:
 	mutex_unlock(&panel->panel_lock);
 	return rc;
@@ -5366,6 +5396,8 @@ int dsi_panel_pre_disable(struct dsi_panel *panel)
 		       panel->name, rc);
 		goto error;
 	}
+
+	PANEL_NOTIFY(PANEL_EVENT_PRE_DISPLAY_OFF);
 
 error:
 	mutex_unlock(&panel->panel_lock);
@@ -5410,6 +5442,8 @@ int dsi_panel_disable(struct dsi_panel *panel)
 	}
 	panel->panel_initialized = false;
 	panel->power_mode = SDE_MODE_DPMS_OFF;
+
+	PANEL_NOTIFY(PANEL_EVENT_DISPLAY_OFF);
 
 	mutex_unlock(&panel->panel_lock);
 	return rc;
