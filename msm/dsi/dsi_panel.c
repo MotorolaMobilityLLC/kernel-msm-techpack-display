4467611ef82b2a68c2f41340e0d8cc8b0d485bd1
litong <litong20@lenovo.com>
2020-03-27 16:44:39 +0800

    Solve the problem of charge mode setting when power off
    Solve the problem that the touchscreen cannot receive the notification
    of the display refresh rate due to Qualcomm rebase.
    
    Change-Id: I09526d5df6056cd7a589f42f90b7734071f9baa5
    Reviewed-on: https://gerrit.mot.com/1522921
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jialei Hao <haojl2@motorola.com>
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- msm/dsi/dsi_panel.c
+++ msm/dsi/dsi_panel.c
@@ -4870,12 +4870,6 @@ int dsi_panel_mode_switch_to_vid(struct dsi_panel *panel)
 		       panel->name, rc);
 
 	mutex_unlock(&panel->panel_lock);
-#if defined(CONFIG_DRM_DYNAMIC_REFRESH_RATE)
-	/* notify consumers only if refresh rate has been updated */
-	if (!rc)
-		blocking_notifier_call_chain(&dsi_freq_head,
-			(unsigned long)panel->cur_mode->timing.refresh_rate, NULL);
-#endif
 	return rc;
 }
 
@@ -4896,6 +4890,13 @@ int dsi_panel_switch(struct dsi_panel *panel)
 		       panel->name, rc);
 
 	mutex_unlock(&panel->panel_lock);
+#if defined(CONFIG_DRM_DYNAMIC_REFRESH_RATE)
+	/* notify consumers only if refresh rate has been updated */
+	if (!rc) {
+		blocking_notifier_call_chain(&dsi_freq_head,
+			(unsigned long)panel->cur_mode->timing.refresh_rate, NULL);
+	}
+#endif
 	return rc;
 }
 
